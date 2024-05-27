63dc6b1ea01b1662d7cfe342e504ccdf9346cc20
inc <87362+inc@users.noreply.github.com>
2024-05-27 11:10:02 +0200

    fixes for tcc

--- buildroot/package/tcc/tcc.mk
+++ buildroot/package/tcc/tcc.mk
@@ -10,17 +10,17 @@ TCC_SITE_METHOD = git
 
 define TCC_BUILD_CMDS
 	# build tcc for riscv32
-	cd $(@D) && ./configure --prefix=$(TARGET_DIR)/usr/local --cpu=riscv32 --cc=$(TARGET_CC) --ar=$(TARGET_AR)
+	cd $(@D) && ./configure --prefix=$(TARGET_DIR)/usr/local --cpu=riscv32 --cc=$(TARGET_CC) --ar=$(TARGET_AR) --extra-cflags=-DCONFIG_TCCDIR='\\\"/usr/local/lib/tcc\\\"'
 	cd $(@D) && $(MAKE) tcc
 	# build tcc cross compiler for riscv32 and build riscv32 library
-	cd $(@D) && ./configure --prefix=$(TARGET_DIR)/usr/local --enable-cross --cpu=riscv32
-	cd $(@D) && $(MAKE) cross-riscv32
+	cd $(@D) && ./configure --prefix=$(TARGET_DIR)/usr/local --enable-cross --cpu=riscv32 --extra-cflags=-DCONFIG_TCCDIR='\\\"/usr/local/lib/tcc\\\"'
+	cd $(@D) && $(MAKE) cross-riscv32-ilp32
 endef
 
 define TCC_INSTALL_TARGET_CMDS
 	cd $(@D) && $(MAKE) install
 	cp $(@D)/../../host/riscv32-buildroot-linux-gnu/sysroot/usr/lib/*crt* $(TARGET_DIR)/usr/lib
-	rm -f $(TARGET_DIR)/usr/local/bin/riscv32-tcc
+	rm -f $(TARGET_DIR)/usr/local/bin/riscv32-ilp32-tcc
 endef
 
 $(eval $(generic-package))

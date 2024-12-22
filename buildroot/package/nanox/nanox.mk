b3115d065566407d421c79bdc8c99aac2c4e0809
machdyne <philip@machdyne.com>
2024-12-22 13:55:44 +0100

    install 9menu

--- buildroot/package/nanox/nanox.mk
+++ buildroot/package/nanox/nanox.mk
@@ -17,7 +17,7 @@ define NANOX_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 -d $(TARGET_DIR)/usr/local/nx
 	$(INSTALL) -m 0755 -d $(TARGET_DIR)/usr/local/nx/bin
 	$(INSTALL) -m 0755 $(@D)/src/bin/* $(TARGET_DIR)/usr/local/nx/bin
-	#$(INSTALL) -m 0755 $(@D)/src/9menu/9menu -t $(TARGET_DIR)/usr/local/nx/bin
+	$(INSTALL) -m 0755 $(@D)/src/9menu/9menu -t $(TARGET_DIR)/usr/local/nx/bin
 	rm -f $(TARGET_DIR)/usr/local/nx/bin/conv*
 	rm -f $(TARGET_DIR)/usr/local/nx/bin/makebmp
 endef
